f5ff226bb6222d196c1f1af56f070300fcc95f14
charlesmclement <46239994+charlesmclement@users.noreply.github.com>
2019-02-06 12:11:33 -0800

    Update cssswitcher.js

--- scripts/cssswitcher.js
+++ scripts/cssswitcher.js
@@ -13,12 +13,10 @@
 }
 
 function menuAbout() {
-    $(document).click(function () {
-        $('#show').click(function () {
-            $('.menu').hide("slide");
-            $('.demos').hide;
-            $('.projects').hide;
-            
-        });
-    });
+    var x = document.getElementById("show");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
